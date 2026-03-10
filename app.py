5ee941c77eaffbd0d93bacbad771f9b3e1d68d66
Nagapure Shweta <133502201+ShwetaNagapure@users.noreply.github.com>
2026-03-10 12:46:28 +0530

    Refactor SocketIO configuration for environment variables
    Updated message queue configuration to use environment variable for REDIS_URL and adjusted port configuration.

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'typing-secret'
 
+import os
+
 socketio = SocketIO(
     app,
-    message_queue="redis://localhost:6379"
+    message_queue=os.environ.get("REDIS_URL")
 )
 
 @app.route('/')
@@ -53,4 +55,5 @@ def handle_stop_typing(data):
     )
 
 if __name__ == "__main__":
-   socketio.run(app, host="0.0.0.0",debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
